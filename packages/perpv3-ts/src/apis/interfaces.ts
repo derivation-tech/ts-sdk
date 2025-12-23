334d491e3a780c9f04bc0b7e87c623b7908b5738
peng-huang-ch <h499871809@gmail.com>
2025-12-23 19:34:51 +0800

    fix: change disableMakerOrderRebate type from true to boolean in FetchMmInstrumentInfoItem interface

--- packages/perpv3-ts/src/apis/interfaces.ts
+++ packages/perpv3-ts/src/apis/interfaces.ts
@@ -581,7 +581,7 @@ export interface FetchMmInstrumentInfoItem {
     base: {
         symbol: string;
     },
-    disableMakerOrderRebate: true,
+    disableMakerOrderRebate: boolean,
     fundingIntervalHour: number;
     initialMarginRatio: number;
     instrumentAddress: string;
