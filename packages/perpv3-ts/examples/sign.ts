d80db52b69c7b1e5af4dce25bdea1693088299c3
peng-huang-ch <h499871809@gmail.com>
2025-12-23 13:52:30 +0800

    refactor: update ApiSign class to use nonce parameter directly
    - Changed the constructor to accept a nonce parameter directly instead of a private variable.
    - Updated the sign method to utilize the nonce for generating the 'X-Api-Nonce' header.

--- packages/perpv3-ts/examples/sign.ts
+++ packages/perpv3-ts/examples/sign.ts
@@ -4,14 +4,14 @@ import type { ApiSigner, SignParams, SignResult } from '../src/apis/interfaces';
  * this is a mock signer for the API examples
  */
 export class ApiSign implements ApiSigner {
-	constructor(private readonly _nonce: string) {}
+	constructor(private readonly nonce: string) {}
 
 	sign(params: SignParams): SignResult {
 		// NOTE: This is a mock signature generation. 
 		// In a real-world scenario, you would use the params and a secret key to generate a valid signature.
 		return {
-			'X-Api-Nonce': 'you should generate a nonce',
-			'X-Api-Sign': 'you should generate a sign',
+			'X-Api-Nonce': this.nonce,
+			'X-Api-Sign': 'you should generate a sign', // this.signer.sign(params)
 			'X-Api-Ts': Date.now(),
 		};
 	}
